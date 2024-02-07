b99fc213bcd06e41cc7aa1f8a913bc34b05fa480
JoyKobia683 <158129484+JoyKobia683@users.noreply.github.com>
2024-02-06 19:36:42 -0800

    Add files via upload

---
+++ file 1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("my name is Joy kobia!\n")
+    printf("my course is civil engineering!\n")
+     printf("My favorite food is rice!\n")
+    printf("my favorite team is man chester united\n")
+
+
+    return 0;
+
+
+
+
+}
