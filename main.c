f62a784d83a0bf5f85d85da678ec7e27316fede9
JoyKobia683 <158129484+JoyKobia683@users.noreply.github.com>
2024-03-10 06:20:36 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -6,9 +6,14 @@ int main(void)
     float a = 0;
     while (a <= 100)
     {
-    printf("%6.2f degrees F = %6.2f degrees C\n",
-        a, (a - 32.0) * 5.0 / 9.0);
-    a = a + 10;
+        if (a > 98.6)
+        {
+           printf("%6.2f degrees F = %6.2f degrees C\n",
+             98.6, (98.6 - 32.0) * 5.0 / 9.0);
+        }
+       printf("%6.2f degrees F = %6.2f degrees C\n",
+           a, (a - 32.0) * 5.0 / 9.0);
+       a = a + 10;
     }
     return 0;
 }
